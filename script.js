d00be83e28698634adc3b4046d2a04aa22d79e1f
Farida-Hamid <faridaaddala@gmail.com>
2022-05-30 17:13:28 +0300

    Creat the funstion to add the books

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-let book = {title: '', author: ''};
 let books = [];
 
+const add = document.querySelector('#add');
+
+add.addEventListener('click', () => {
+  title = document.querySelector('#title');
+  author = document. querySelector('#author');
+  books.push({title, author});
+});
 
 
+/// local storage
+const title = document.querySelector('#title');
+const author = document. querySelector('#author');
+const saver = JSON.stringify({ title, author});
