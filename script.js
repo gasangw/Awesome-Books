7f53990e20d37f22a45b5b2b2357ff4fc85181cc
Farida-Hamid <faridaaddala@gmail.com>
2022-05-30 17:28:44 +0300

    Adjust function and event listener

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 let books = [];
+console.log('INITINALIZED', books);
+
+function addBook(title, author){
+  console.log("function called")
+  books.push({title, author});
+  console.log("book added", books);
+}
 
 const add = document.querySelector('#add');
 
-add.addEventListener('click', () => {
+add.addEventListener('onclick', () => {
   title = document.querySelector('#title');
   author = document. querySelector('#author');
-  books.push({title, author});
+  addBook(title, author);
 });
 
-
 /// local storage
-const title = document.querySelector('#title');
-const author = document. querySelector('#author');
-const saver = JSON.stringify({ title, author});
+// const title = document.querySelector('#title');
+// const author = document. querySelector('#author');
+// const saver = JSON.stringify({ title, author});
